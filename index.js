b5590747189f8cb13e0a4d3ce796e57d67b82af8
Darren L <lgytuan@gmail.com>
2025-04-03 08:53:50 +0000

    try to build the database

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const db = {};
+
+const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri);
+
+
+const userSchema = new mongoose.Schema({
+  username : { type : String, required : true}
+});
+
+const logSchema = new mongoose.Schema({
+  description : { type: String, required : true},
+  duration : { type : Number, required : true},
+  date : { type : Date, default : Date.now}
+});
+
+const dbSchema = new mongoose.Schema({
+  username : { type : String, required : true},
+  description : { type: String, required : true},
+  duration : { type : Number, required : true},
+  date : { type : Date, default : Date.now},
+  count : { type : Number, default : 0},
+  log : { type : [logSchema]}
+});
+
+const trackerModel = mongoose.model("trackerModel", dbSchema); 
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,8 +38,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-console.log("test");
+console.log("start check");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.post('/api/users', (req,res) => {
+  const inputUsername = req.body.username;
+  console.log(inputUsername);
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
